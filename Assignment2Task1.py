04c05b10922e9626f97ec46fe09c81ac79d06ede
pallantinag <avexa.nag@gmail.com>
2025-04-09 22:09:09 +0530

    Asignment2 completed

---
+++ Assignment2Task1.py
@@ -0,0 +1,6 @@
+a=int(input("Enter a number: "))
+
+if (a%2==0):
+  print(str(a)+ " is an even number")
+else:
+    print(str(a) + " is an odd number")
---
+++ Assignment2Task2.py
@@ -0,0 +1,5 @@
+a= int(0)
+for i in range(1,51):
+    a=a+i
+print("the sum of the numbers from 1 to 50 is:"+str(a))
+
